9ce43b82e2ce89aab1d783cac3e7628cd496d805
orafik <123893171+orafik@users.noreply.github.com>
2023-01-29 13:57:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.header( 'Breakfast Favorites')
 streamlit.text( ' Omega 3 & Blueberry Oatmeal' )
 streamlit.text( 'Kale, Spinach & Rocket Smoothie')
 streamlit.text( 'Hard-Boiled Freee-Range Egg' )
+
+
+streamlit.header ('Build Your Own Fruit Smoothie')
