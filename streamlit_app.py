1a4df06b967868ca8195fbbd99817d52a267b7fa
orafik <123893171+orafik@users.noreply.github.com>
2023-01-29 13:10:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title( 'My Parents New Healthy Diner' )
 streamlit.header( 'Breakfast Menu')
 streamlit.text( 'Omega 3 & Blueberry Oatmeal' )
-Streamlit.text( 'Kale, Spinach & Rocket Smoothie')
+streamlit.text( 'Kale, Spinach & Rocket Smoothie')
 streamlit.text( 'Hard-Boiled Freee-Range Egg' )
